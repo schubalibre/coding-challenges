8290f843363432d446af0282b8ddc0847b28b33c
Robert Dziuba <robertodziuba@gmail.com>
2019-09-25 23:14:07 +0200

    Interface for inquiry updates implemented

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -2,15 +2,27 @@ package com.mhp.coding.challenges.dependency.inquiry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class InquiryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
+    private final List<InquiryServiceInterface> inquiryService;
+
+    @Autowired
+    public InquiryService(List<InquiryServiceInterface> inquiryService) {
+        this.inquiryService = inquiryService;
+    }
 
     public void create(final Inquiry inquiry) {
-        LOG.info("User sent inquiry: {}", inquiry);
+        inquiryService.forEach(service -> {
+            service.notify(inquiry);
+            LOG.info("Transferred {} to service {}", inquiry, service);
+        });
     }
 
 }
